a760a3a9000b64eb5372b42240bbd4a32d1b5a62
ElnurKasimov <ekasimov2012@gmail.com>
2024-09-27 08:11:28 +0300

    changed a main class

--- src/main/java/com/binance/trading/TradingApplication.java
+++ src/main/java/com/binance/trading/TradingApplication.java
@@ -3,6 +3,7 @@ package com.binance.trading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TradingApplication {
 
